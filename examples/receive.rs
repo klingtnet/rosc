cbb749fb70f253bce3440daabb60d90cd46dc84c
Andreas Linz <klingt.net@gmail.com>
2015-10-23 15:12:30 +0200

    Refactor address and port parsing to single method
    - lib should only return errors but should not panic

--- examples/receive.rs
+++ examples/receive.rs
@@ -4,18 +4,17 @@ use std::{net, env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        println!("Usage ./receive IP PORT");
+    if args.len() < 2 {
+        println!("Usage ./receive IP:PORT");
         process::exit(1);
     }
 
-    let addr: net::Ipv4Addr = rosc::utils::parse_ip_v4(&args[1]);
-    let port: u16 = args[2]
-                        .parse()
-                        .ok()
-                        .expect("PORT must be in range [0, 65535]!");
-
-    let sock = net::UdpSocket::bind((addr, port));
-    println!("Listening to {}:{}", addr, port);
-    drop(sock);
+    match rosc::utils::parse_ip_and_port(&args[1]) {
+        Ok((ip, port)) => {
+            let sock = net::UdpSocket::bind((ip, port));
+            println!("Listening to {}:{}", ip, port);
+            drop(sock);
+        }
+        Err(e) => println!("{}", e),
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,44 @@
-use std::net;
+use std::{net, num};
 
-pub fn parse_ip_v4(raw_addr: &String) -> net::Ipv4Addr {
+fn parse_ip_v4(raw_addr: &str) -> Result<net::Ipv4Addr, String> {
     let octets: Vec<u8> = raw_addr.splitn(4, '.')
-                                  .map(|octet| {
-                                      octet.parse::<u8>()
-                                           .ok()
-                                           .expect("Octet must be in range [0, 255]")
+                                  .map(|octet| octet.parse::<u8>())
+                                  .filter_map(|octet| {
+                                      if octet.is_ok() {
+                                          octet.ok()
+                                      } else {
+                                          None
+                                      }
                                   })
                                   .collect();
-    net::Ipv4Addr::new(octets[0], octets[1], octets[2], octets[3])
+
+    if octets.len() < 4 {
+        Err(format!("There was an error parsing the IPv4 address: {}", raw_addr))
+    } else {
+        Ok(net::Ipv4Addr::new(octets[0], octets[1], octets[2], octets[3]))
+    }
+}
+
+fn parse_port(raw_port: &str) -> Result<u16, num::ParseIntError> {
+    raw_port.parse()
+}
+
+pub fn parse_ip_and_port(raw_addr: &String) -> Result<(net::Ipv4Addr, u16), String> {
+    let parts: Vec<&str> = raw_addr.splitn(2, ':').collect();
+    match parts.len() {
+        2 => {
+            let ip: Result<net::Ipv4Addr, String> = parse_ip_v4(parts[0]);
+            let port: Result<u16, num::ParseIntError> = parse_port(parts[1]);
+            match ip {
+                Ok(ip) => {
+                    match port {
+                        Ok(port) => Ok((ip, port)),
+                        Err(e) => Err(format!("Bad port value: {}", e)),
+                    }
+                }
+                Err(e) => Err(format!("Bad IPv4 address: {}", e)),
+            }
+        }
+        _ => Err(format!("Bad address: {}, Missing port?", raw_addr)),
+    }
 }
