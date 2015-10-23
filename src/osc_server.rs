a6ff538259979c7383fb3c45345aab6b5de3bbe3
Andreas Linz <klingt.net@gmail.com>
2015-10-23 20:26:49 +0200

    Add `destruct` method
    - Tries to destruct an UDP message as `OscPacket`

--- src/osc_server.rs
+++ src/osc_server.rs
@@ -1,2 +1,8 @@
+use {osc_types, errors};
+
 /// Common MTP size for ethernet
 pub const MTP: usize = 1536;
+
+pub fn destruct(message: &mut [u8], size: usize) -> Result<osc_types::OscPacket, errors::OscError> {
+    Ok(osc_types::OscPacket::OscMessage)
+}
