7f18bf70629e22a5186e84918293693244e63e74
Andreas Linz <klingt.net@gmail.com>
2015-10-26 13:36:36 +0100

    Mock implementation of Display trait for OscError

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,17 @@
+use std::fmt;
+
 pub enum OscError {
     BadOscPacket(String),
     BadOscAddress(String),
     BadOscMessage(String),
     BadOscString(String),
     BadOscBundle,
+}
+
+impl fmt::Display for OscError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            _ => write!(f, "{}", "TODO"),
+        }
+    }
 }
