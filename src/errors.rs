6eda1143ea9e8081b57b5a53601a395b7ba3471f
Andreas Linz <klingt.net@gmail.com>
2015-10-31 15:05:25 +0100

    Add byteorder Error
    - TODO: merge error types

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,10 @@
+use byteorder;
 use std::{fmt, io, string};
 
 pub enum OscError {
     StringError(string::FromUtf8Error),
     ReadError(io::Error),
+    ByteOrderError(byteorder::Error),
     BadOscPacket(String),
     BadOscAddress(String),
     BadOscMessage(String),
