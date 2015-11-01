3ea17c12495c318b0273ff3513d8409f00cea8c1
Andreas Linz <klingt.net@gmail.com>
2015-11-01 19:06:57 +0100

    Add a test for the osc decoder

---
+++ tests/decoder_test.rs
@@ -0,0 +1,35 @@
+extern crate rosc;
+
+use rosc::{types, errors, decoder, utils};
+
+#[test]
+fn test_decode() {
+    let raw_addr = "/some/valid/address/4";
+    let addr = pad(raw_addr.as_bytes());
+    // args
+    let type_tags = pad(b",");
+    let merged: Vec<u8> = addr.into_iter()
+        .chain(type_tags.into_iter())
+        .collect();
+    let osc_packet: Result<types::OscPacket, errors::OscError> = decoder::decode(&merged);
+    assert!(osc_packet.is_ok());
+    match osc_packet {
+        Ok(packet) => match packet {
+            types::OscPacket::Message(msg) => {
+                assert_eq!(raw_addr, msg.addr);
+                assert!(msg.args.is_none());
+            },
+            _ => panic!()
+        },
+        Err(e) => panic!(e)
+    }
+}
+
+fn pad(data: &[u8]) -> Vec<u8> {
+    let pad_len: usize = utils::pad(data.len() as u64) as usize;
+    let mut v: Vec<u8> = data.iter().cloned().collect();
+    while v.len() < pad_len {
+        v.push(0u8);
+    }
+    v
+}
