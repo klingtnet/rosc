b74b478bc97612e4feb6e6deaf42cd9eb50909f0
Andreas Linz <klingt.net@gmail.com>
2015-11-01 17:51:31 +0100

    Extracted four byte padding function to `utils`

--- src/utils.rs
+++ src/utils.rs
@@ -56,3 +56,19 @@ pub fn parse_ip_and_port(raw_addr: &String) -> Result<(net::Ipv4Addr, u16), Stri
         _ => Err(format!("Bad address: {}, Missing port?", raw_addr)),
     }
 }
+
+/// Returns the 4-byte padded position
+///
+/// # Example
+///
+/// ```
+/// use rosc::utils;
+/// let pos: u64 = 10;
+/// assert_eq!(12u64, utils::pad(pos))
+/// ```
+pub fn pad(pos: u64) -> u64 {
+    match pos % 4 {
+        0 => 0,
+        d => pos + (4-d)
+    }
+}
