bde0aaad03df16bdffe1eb1898294fef46410c24
Andreas Linz <klingt.net@gmail.com>
2015-11-01 11:39:27 +0100

    Rustfmt

--- src/osc_decoder.rs
+++ src/osc_decoder.rs
@@ -1,5 +1,4 @@
-use {osc_types as ot,
-    errors as oe};
+use {osc_types as ot, errors as oe};
 
 use std::{io, string, mem, error};
 use std::io::{Read, BufRead};
@@ -33,20 +32,20 @@ fn decode_message(msg: &[u8], size: usize) -> ot::OscResult<ot::OscPacket> {
                     if type_tags.len() > 1 {
                         match read_osc_args(&mut cursor, type_tags) {
                             Ok(args) => {
-                                Ok(ot::OscPacket::Message(ot::OscMessage{
+                                Ok(ot::OscPacket::Message(ot::OscMessage {
                                     addr: addr,
                                     args: Some(args),
                                 }))
-                            },
+                            }
                             Err(e) => Err(e),
                         }
                     } else {
-                        Ok(ot::OscPacket::Message(ot::OscMessage{
+                        Ok(ot::OscPacket::Message(ot::OscMessage {
                             addr: addr,
-                            args: None
+                            args: None,
                         }))
                     }
-                },
+                }
                 Err(e) => Err(e),
             }
         }
@@ -64,23 +63,25 @@ fn read_osc_string(cursor: &mut io::Cursor<&[u8]>) -> ot::OscResult<String> {
             String::from_utf8(str_buf)
                 .map_err(|e| oe::OscError::StringError(e))
                 .map(|s| s.trim_matches(0u8 as char).to_string())
-        },
+        }
         Err(e) => Err(oe::OscError::ReadError(e)),
     }
 }
 
-fn read_osc_args(cursor: &mut io::Cursor<&[u8]>, raw_type_tags: String) -> ot::OscResult<Vec<ot::OscType>> {
+fn read_osc_args(cursor: &mut io::Cursor<&[u8]>,
+                 raw_type_tags: String)
+                 -> ot::OscResult<Vec<ot::OscType>> {
     let type_tags: Vec<char> = raw_type_tags.chars()
-        .skip(1)
-        .map(|c| c as char)
-        .collect();
+                                            .skip(1)
+                                            .map(|c| c as char)
+                                            .collect();
     let mut args: Vec<ot::OscType> = Vec::with_capacity(type_tags.len());
     for tag in type_tags {
         match read_osc_arg(cursor, tag) {
             Ok(arg) => {
                 args.push(arg);
-            },
-            Err(e) => return Err(e)
+            }
+            Err(e) => return Err(e),
         }
     }
     Ok(args)
@@ -90,28 +91,27 @@ fn read_osc_arg(cursor: &mut io::Cursor<&[u8]>, tag: char) -> ot::OscResult<ot::
     match tag {
         'f' => {
             cursor.read_f32::<BigEndian>()
-            .map(|f| ot::OscType::OscFloat(f))
-            .map_err(|e| oe::OscError::ByteOrderError(e))
-        },
+                  .map(|f| ot::OscType::OscFloat(f))
+                  .map_err(|e| oe::OscError::ByteOrderError(e))
+        }
         'd' => {
             cursor.read_f64::<BigEndian>()
-            .map(|d| ot::OscType::OscDouble(d))
-            .map_err(|e| oe::OscError::ByteOrderError(e))
+                  .map(|d| ot::OscType::OscDouble(d))
+                  .map_err(|e| oe::OscError::ByteOrderError(e))
         }
         'i' => {
             cursor.read_i32::<BigEndian>()
-            .map(|i| ot::OscType::OscInt(i))
-            .map_err(|e| oe::OscError::ByteOrderError(e))
-        },
+                  .map(|i| ot::OscType::OscInt(i))
+                  .map_err(|e| oe::OscError::ByteOrderError(e))
+        }
         'h' => {
             cursor.read_i64::<BigEndian>()
-            .map(|l| ot::OscType::OscLong(l))
-            .map_err(|e| oe::OscError::ByteOrderError(e))
-        },
+                  .map(|l| ot::OscType::OscLong(l))
+                  .map_err(|e| oe::OscError::ByteOrderError(e))
+        }
         's' => {
-            read_osc_string(cursor)
-            .map(|s| ot::OscType::OscString(s))
-        },
+            read_osc_string(cursor).map(|s| ot::OscType::OscString(s))
+        }
         't' => {
             // http://opensoundcontrol.org/node/3/#timetags
             read_time_tag(cursor)
@@ -122,12 +122,13 @@ fn read_osc_arg(cursor: &mut io::Cursor<&[u8]>, tag: char) -> ot::OscResult<ot::
                     let mut buf: Vec<u8> = Vec::with_capacity(size as usize);
                     match cursor.take(size as u64).read(&mut buf) {
                         Ok(blob_size) => Ok(ot::OscType::OscBlob(buf)),
-                        Err(e) => Err(oe::OscError::ReadError(e))
+                        Err(e) => Err(oe::OscError::ReadError(e)),
                     }
-                },
-                Err(e) => return Err(oe::OscError::BadOscBundle)
+                }
+                // TODO: use correct error type
+                Err(e) => return Err(oe::OscError::BadOscBundle),
             }
-        },
+        }
         _ => Err(oe::OscError::BadOscArg(format!("Type tag \"{}\" is not implemented!", tag))),
     }
 }
@@ -137,10 +138,10 @@ fn read_time_tag(cursor: &mut io::Cursor<&[u8]>) -> ot::OscResult<ot::OscType> {
         Ok(date) => {
             match cursor.read_u32::<BigEndian>() {
                 Ok(frac) => Ok(ot::OscType::OscTime(date, frac)),
-                Err(e) => Err(oe::OscError::ByteOrderError(e))
+                Err(e) => Err(oe::OscError::ByteOrderError(e)),
             }
-        },
-        Err(e) => Err(oe::OscError::ByteOrderError(e))
+        }
+        Err(e) => Err(oe::OscError::ByteOrderError(e)),
     }
 }
 
