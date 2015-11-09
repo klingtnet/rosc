6db84c0bd7b9ffe55d413be3a58e144933ffdb7d
Andreas Linz <klingt.net@gmail.com>
2015-11-09 21:20:55 +0100

    Directly use `encode_message`, see commit 0fa8250 description

--- tests/encoder_test.rs
+++ tests/encoder_test.rs
@@ -11,7 +11,7 @@ fn test_encode_message_wo_args() {
         args: None,
     };
 
-    let enc_msg = encoder::encode_packet(&OscPacket::Message(msg).unwrap();
+    let enc_msg = encoder::encode_message(&msg).unwrap();
     match decoder::decode(&enc_msg).unwrap() {
         OscPacket::Message(dec_msg) => assert_eq!("/some/addr", dec_msg.addr),
         _ => panic!("Expected OSC message!")
