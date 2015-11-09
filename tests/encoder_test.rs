8eb2348e75c773711c1fa538708b5307eec51b85
Andreas Linz <klingt.net@gmail.com>
2015-11-09 21:22:04 +0100

    Check agains `addr` field of source `msg`

--- tests/encoder_test.rs
+++ tests/encoder_test.rs
@@ -13,7 +13,7 @@ fn test_encode_message_wo_args() {
 
     let enc_msg = encoder::encode_message(&msg).unwrap();
     match decoder::decode(&enc_msg).unwrap() {
-        OscPacket::Message(dec_msg) => assert_eq!("/some/addr", dec_msg.addr),
-        _ => panic!("Expected OSC message!")
+        OscPacket::Message(dec_msg) => assert_eq!(msg.addr, dec_msg.addr),
+        _ => panic!("Expected OSC message!"),
     }
 }
