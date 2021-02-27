b267167805648186c2959ccf8e2dbda9e9e3cfc6
Andreas Linz <klingt.net@gmail.com>
2021-02-27 13:59:13 +0100

    Add a regression test for FromIterator of OscArray

---
+++ tests/types_test.rs
@@ -0,0 +1,16 @@
+extern crate rosc;
+
+use rosc::{OscArray, OscType};
+
+#[test]
+fn test_osc_array_from_iter() {
+    use std::iter::FromIterator;
+    let iter = (0..3).map(|i| OscType::Int(i));
+    let osc_arr = OscArray::from_iter(iter);
+    assert_eq!(
+        osc_arr,
+        OscArray {
+            content: vec![OscType::Int(0), OscType::Int(1), OscType::Int(2)]
+        }
+    );
+}
