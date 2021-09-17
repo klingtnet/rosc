21c218b21cbe184be7718ceb64a3739f6f9e4018
Lukas Jackowski <l.jackowski@agorate.de>
2021-09-17 20:59:11 +0200

    Add validate_message_address method

--- src/address.rs
+++ src/address.rs
@@ -29,3 +29,67 @@ pub fn validate_method_address(addr: &String) -> Result<()>
 
     return Ok(());
 }
+
+/// Check if the address of an OSC message is valid
+pub fn validate_message_address(addr: &String) -> Result<()>
+{
+    if !addr.is_ascii() {
+        return Err(OscError::BadAddress("Address must only contain ASCII characters"));
+    }
+    if addr.len() < 1 {
+        return Err(OscError::BadAddress("Address must be at least 1 character long"));
+    }
+    let mut chars = addr.chars();
+
+    // Check if address starts with '/'
+    let first = chars.next().unwrap();
+    if first != '/' {
+        return Err(OscError::BadAddress("Address must start with '/'"));
+    }
+
+    // Validate rest of address
+    let mut in_character_range = false;
+    let mut in_string_list = false;
+    for char in chars
+    {
+        if " #,".chars().any(|x| char == x) {
+            return Err(OscError::BadAddress("Address may not contain any of the following characters: ' #,'"));
+        }
+        if char == '[' {
+            in_character_range = true;
+            if in_string_list {
+                return Err(OscError::BadAddress("Can not start a character range (square brackets) within string list (curly brackets)"));
+            }
+        }
+        if (char == '/') & in_character_range {
+            return Err(OscError::BadAddress("Character range (square brackets) was started but not closed before the next address part started"));
+        }
+        if char == ']' {
+            in_character_range = false;
+        }
+        if char == '{' {
+            in_string_list = true;
+        }
+        if (char == '/') & in_string_list {
+            return Err(OscError::BadAddress("String list (curly brackets) was started but not closed before the next address part started"));
+        }
+        if char == '}' {
+            in_string_list = false;
+            if in_character_range {
+                return Err(OscError::BadAddress("Can not start a string list (curly brackets) within character range (square brackets)"));
+            }
+        }
+    }
+    if in_character_range {
+        return Err(OscError::BadAddress("Character range (square brackets) was started but not closed before the end of the address"));
+    }
+    if in_string_list {
+        return Err(OscError::BadAddress("String list (curly brackets) was started but not closed before the end of the address"));
+    }
+
+
+    // TODO: Check if non-printable ascii characters are contained?
+
+
+    return Ok(());
+}
--- tests/address_test.rs
+++ tests/address_test.rs
@@ -26,3 +26,67 @@ fn test_validate_method_address() {
         _ => assert!(false)
     }
 }
+
+#[test]
+fn test_validate_message_address() {
+    // Valid addresses
+    address::validate_message_address(&String::from("/a"));
+
+    // Invalid addresses
+    match address::validate_message_address(&String::from("öäü")).err().expect("") {
+        OscError::BadAddress("Address must only contain ASCII characters") => assert!(true),
+        _ => assert!(false)
+    }
+    match address::validate_message_address(&String::from("")).err().expect("") {
+        OscError::BadAddress("Address must be at least 1 character long") => assert!(true),
+        _ => assert!(false)
+    }
+    match address::validate_message_address(&String::from("foo")).err().expect("") {
+        OscError::BadAddress("Address must start with '/'") => assert!(true),
+        _ => assert!(false)
+    }
+
+    // Square brackets open and never close
+    match address::validate_message_address(&String::from("/[a/b")).err().expect("") {
+        OscError::BadAddress("Character range (square brackets) was started but not closed before the next address part started") => assert!(true),
+        _ => assert!(false)
+    }
+    // Square brackets contains /
+    match address::validate_message_address(&String::from("/[a/]b")).err().expect("") {
+        OscError::BadAddress("Character range (square brackets) was started but not closed before the next address part started") => assert!(true),
+        _ => assert!(false)
+    }
+    // Square brackets open but don't close before the address ends
+    match address::validate_message_address(&String::from("/[a")).err().expect("") {
+        OscError::BadAddress("Character range (square brackets) was started but not closed before the end of the address") => assert!(true),
+        _ => assert!(false)
+    }
+
+    // Curly brakcets open and never close
+    match address::validate_message_address(&String::from("/{a/b")).err().expect("") {
+        OscError::BadAddress("String list (curly brackets) was started but not closed before the next address part started") => assert!(true),
+        _ => assert!(false)
+    }
+    // Curly brackets contains /
+    match address::validate_message_address(&String::from("/{a/}b")).err().expect("") {
+        OscError::BadAddress("String list (curly brackets) was started but not closed before the next address part started") => assert!(true),
+        _ => assert!(false)
+    }
+    // Curly brackets open but don't close before the address ends
+    match address::validate_message_address(&String::from("/{a")).err().expect("") {
+        OscError::BadAddress("String list (curly brackets) was started but not closed before the end of the address") => assert!(true),
+        _ => assert!(false)
+    }
+
+    // Curly brackets open but don't close before the address ends
+    match address::validate_message_address(&String::from("/{a[foo]}/")).err().expect("") {
+        OscError::BadAddress("Can not start a character range (square brackets) within string list (curly brackets)") => assert!(true),
+        _ => assert!(false)
+    }
+
+    // Square brackets within curly brackets
+    match address::validate_message_address(&String::from("/[a{foo}]/")).err().expect("") {
+        OscError::BadAddress("Can not start a string list (curly brackets) within character range (square brackets)") => assert!(true),
+        _ => assert!(false)
+    }
+}
