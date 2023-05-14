2a3dc78f4c7511015e201bb12c14af9cad33a543
Andreas Linz <klingt.net@gmail.com>
2023-05-14 19:25:17 +0200

    Rework encoder tests
    This makes the naming consistent by using packet as the name for the
    OSC packet to be encoded, and bytes as the name for vector of bytes that
    contains the encoded packet.  Besides renaming of variables this also
    simplifies the identity check by not unwrapping the packet to compare
    the contents, and instead asserting that the packet itself is equal.

--- tests/encoder_test.rs
+++ tests/encoder_test.rs
@@ -14,54 +14,38 @@ fn test_pad() {
 
 #[test]
 fn test_encode_message_wo_args() {
-    let msg_packet = OscPacket::Message(OscMessage {
+    let packet = OscPacket::Message(OscMessage {
         addr: "/some/addr".to_string(),
         args: vec![],
     });
 
-    let enc_msg = encoder::encode(&msg_packet).unwrap();
-    assert_eq!(enc_msg.len() % 4, 0);
+    let bytes = encoder::encode(&packet).unwrap();
+    assert_eq!(bytes.len() % 4, 0);
 
-    let msg = match msg_packet {
-        OscPacket::Message(ref msg) => msg,
-        _ => panic!(),
-    };
-
-    let dec_msg = match decoder::decode_udp(&enc_msg).unwrap().1 {
-        OscPacket::Message(m) => m,
-        _ => panic!("Expected OscMessage!"),
-    };
-
-    assert_eq!(*msg, dec_msg)
+    let (tail, decoded_packet) = decoder::decode_udp(&bytes).expect("decode failed");
+    assert_eq!(0, tail.len());
+    assert_eq!(packet, decoded_packet)
 }
 
 #[test]
 fn test_encode_empty_bundle() {
-    let bundle_packet = OscPacket::Bundle(OscBundle {
+    let packet = OscPacket::Bundle(OscBundle {
         timetag: (4, 2).into(),
         content: vec![],
     });
 
-    let enc_bundle = encoder::encode(&bundle_packet).unwrap();
-    assert_eq!(enc_bundle.len() % 4, 0);
-    assert_eq!(enc_bundle.len(), 16);
+    let bytes = encoder::encode(&packet).unwrap();
+    assert_eq!(bytes.len() % 4, 0);
+    assert_eq!(bytes.len(), 16);
 
-    let dec_bundle = match decoder::decode_udp(&enc_bundle).unwrap().1 {
-        OscPacket::Bundle(m) => m,
-        _ => panic!("Expected OscBundle!"),
-    };
-
-    let bundle = match bundle_packet {
-        OscPacket::Bundle(ref bundle) => bundle,
-        _ => panic!(),
-    };
-
-    assert_eq!(*bundle, dec_bundle)
+    let (tail, decoded_packet) = decoder::decode_udp(&bytes).expect("decode failed");
+    assert_eq!(0, tail.len());
+    assert_eq!(packet, decoded_packet)
 }
 
 #[test]
 fn test_encode_message_with_args() {
-    let msg_packet = OscPacket::Message(OscMessage {
+    let packet = OscPacket::Message(OscMessage {
         addr: "/another/address/1".to_string(),
         args: vec![
             4i32.into(),
@@ -105,110 +89,88 @@ fn test_encode_message_with_args() {
         ],
     });
 
-    let enc_msg = encoder::encode(&msg_packet).unwrap();
-    assert_eq!(enc_msg.len() % 4, 0);
+    let bytes = encoder::encode(&packet).unwrap();
+    assert_eq!(bytes.len() % 4, 0);
 
-    let dec_msg: OscMessage = match decoder::decode_udp(&enc_msg).unwrap().1 {
-        OscPacket::Message(m) => m,
-        _ => panic!("Expected OscMessage!"),
-    };
-
-    let msg = match msg_packet {
-        OscPacket::Message(ref msg) => msg,
-        _ => panic!(),
-    };
-
-    assert_eq!(*msg, dec_msg);
+    let (tail, decoded_packet) = decoder::decode_udp(&bytes).expect("decode failed");
+    assert_eq!(0, tail.len());
+    assert_eq!(packet, decoded_packet)
 }
 
 #[test]
 fn test_encode_bundle() {
-    let msg0 = OscMessage {
-        addr: "/view/1".to_string(),
-        args: vec![],
-    };
-
-    let msg1 = OscMessage {
-        addr: "/mixer/channel/1/amp".to_string(),
-        args: vec![0.9f32.into()],
-    };
-
-    let msg2 = OscMessage {
-        addr: "/osc/1/freq".to_string(),
-        args: vec![440i32.into()],
-    };
-
-    let msg3 = OscMessage {
-        addr: "/osc/1/phase".to_string(),
-        args: vec![(-0.4f32).into()],
-    };
-
-    let bundle1 = OscBundle {
-        timetag: (5678, 8765).into(),
-        content: vec![OscPacket::Message(msg2), OscPacket::Message(msg3)],
-    };
-
-    let root_bundle = OscPacket::Bundle(OscBundle {
+    let packet = OscPacket::Bundle(OscBundle {
         timetag: (1234, 4321).into(),
         content: vec![
-            OscPacket::Message(msg0),
-            OscPacket::Message(msg1),
-            OscPacket::Bundle(bundle1),
+            OscPacket::Message(OscMessage {
+                addr: "/view/1".to_string(),
+                args: vec![],
+            }),
+            OscPacket::Message(OscMessage {
+                addr: "/mixer/channel/1/amp".to_string(),
+                args: vec![0.9f32.into()],
+            }),
+            OscPacket::Bundle(OscBundle {
+                timetag: (5678, 8765).into(),
+                content: vec![
+                    OscPacket::Message(OscMessage {
+                        addr: "/osc/1/freq".to_string(),
+                        args: vec![440i32.into()],
+                    }),
+                    OscPacket::Message(OscMessage {
+                        addr: "/osc/1/phase".to_string(),
+                        args: vec![(-0.4f32).into()],
+                    }),
+                ],
+            }),
         ],
     });
 
-    let enc_bundle = encoder::encode(&root_bundle).unwrap();
-    assert_eq!(enc_bundle.len() % 4, 0);
+    let bytes = encoder::encode(&packet).unwrap();
+    assert_eq!(bytes.len() % 4, 0);
 
-    let dec_bundle = decoder::decode_udp(&enc_bundle).unwrap().1;
-    assert_eq!(root_bundle, dec_bundle);
+    let decoded_packet = decoder::decode_udp(&bytes).unwrap().1;
+    assert_eq!(packet, decoded_packet);
 }
 
 #[cfg(feature = "std")]
 #[test]
 fn test_encode_bundle_into_cursor() {
-    let msg0 = OscMessage {
-        addr: "/view/1".to_string(),
-        args: vec![],
-    };
-
-    let msg1 = OscMessage {
-        addr: "/mixer/channel/1/amp".to_string(),
-        args: vec![0.9f32.into()],
-    };
-
-    let msg2 = OscMessage {
-        addr: "/osc/1/freq".to_string(),
-        args: vec![440i32.into()],
-    };
-
-    let msg3 = OscMessage {
-        addr: "/osc/1/phase".to_string(),
-        args: vec![(-0.4f32).into()],
-    };
-
-    let bundle1 = OscBundle {
-        timetag: (5678, 8765).into(),
-        content: vec![OscPacket::Message(msg2), OscPacket::Message(msg3)],
-    };
-
-    let root_bundle = OscPacket::Bundle(OscBundle {
+    let packet = OscPacket::Bundle(OscBundle {
         timetag: (1234, 4321).into(),
         content: vec![
-            OscPacket::Message(msg0),
-            OscPacket::Message(msg1),
-            OscPacket::Bundle(bundle1),
+            OscPacket::Message(OscMessage {
+                addr: "/view/1".to_string(),
+                args: vec![],
+            }),
+            OscPacket::Message(OscMessage {
+                addr: "/mixer/channel/1/amp".to_string(),
+                args: vec![0.9f32.into()],
+            }),
+            OscPacket::Bundle(OscBundle {
+                timetag: (5678, 8765).into(),
+                content: vec![
+                    OscPacket::Message(OscMessage {
+                        addr: "/osc/1/freq".to_string(),
+                        args: vec![440i32.into()],
+                    }),
+                    OscPacket::Message(OscMessage {
+                        addr: "/osc/1/phase".to_string(),
+                        args: vec![(-0.4f32).into()],
+                    }),
+                ],
+            }),
         ],
     });
 
-    let mut buffer = Vec::new();
+    let mut bytes = Vec::new();
     encoder::encode_into(
-        &root_bundle,
-        &mut encoder::WriteOutput(std::io::Cursor::new(&mut buffer)),
+        &packet,
+        &mut encoder::WriteOutput(std::io::Cursor::new(&mut bytes)),
     )
     .unwrap();
-    assert_eq!(buffer.len() % 4, 0);
+    assert_eq!(bytes.len() % 4, 0);
 
-    let dec_bundle = decoder::decode_udp(&buffer).unwrap().1;
-    assert_eq!(root_bundle, dec_bundle);
+    let decoded_packet = decoder::decode_udp(&bytes).unwrap().1;
+    assert_eq!(packet, decoded_packet);
 }
