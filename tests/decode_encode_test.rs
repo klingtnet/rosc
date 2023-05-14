292e00bfe6f70cb85ae20f4c490dae7e5057bcee
Andreas Linz <klingt.net@gmail.com>
2023-05-14 20:48:30 +0200

    Merge encoder and decoder tests
    This reduces redundancy by testing encoding and decoding in the same
    unit test by asserting identity. Also, golden values were added as a
    reference to check encoding against. Those values were generated by hex
    encoding the bytes returned by the current encoder implementation.

--- tests/decode_encode_test.rs
+++ tests/decode_encode_test.rs
@@ -1,26 +1,24 @@
 extern crate rosc;
 
-use rosc::encoder::pad;
 use rosc::{decoder, encoder};
 use rosc::{OscArray, OscBundle, OscColor, OscMessage, OscMidiMessage, OscPacket, OscType};
 
-#[test]
-fn test_pad() {
-    assert_eq!(4, pad(4));
-    assert_eq!(8, pad(5));
-    assert_eq!(8, pad(6));
-    assert_eq!(8, pad(7));
-}
+extern crate hex;
+
+const GOLDEN_MESSAGE_WO_ARGS: &str = "2f736f6d652f6164647200002c000000";
+const GOLDEN_MESSAGE_WITH_ALL_TYPES: &str = "2f616e6f746865722f616464726573732f3100002c69686664737362746346544e496d725b695b64645d735d0000000000000004000000000000002a40490fda400921fb54442eea54686973206973206120737472696e672e00000054686973206973206120737472696e6720746f6f2e00000000000003010203000000007b000001c80000006304292a81ffc02a0d0000002a3ff3ae147ae147ae4009ae147ae147ae59617900";
+const GOLDEN_EMPTY_BUNDLE: &str = "2362756e646c65000000000400000002";
+const GOLDEN_BUNDLE: &str = "2362756e646c6500000004d2000010e10000000c2f766965772f31002c000000000000202f6d697865722f6368616e6e656c2f312f616d70000000002c6600003f666666000000442362756e646c65000000162e0000223d000000142f6f73632f312f66726571002c690000000001b8000000182f6f73632f312f7068617365000000002c660000becccccd";
 
 #[test]
-fn test_encode_message_wo_args() {
+fn test_message_wo_args() {
     let packet = OscPacket::Message(OscMessage {
         addr: "/some/addr".to_string(),
         args: vec![],
     });
 
-    let bytes = encoder::encode(&packet).unwrap();
-    assert_eq!(bytes.len() % 4, 0);
+    let bytes = encoder::encode(&packet).expect("encode failed");
+    assert_eq!(hex::decode(GOLDEN_MESSAGE_WO_ARGS).unwrap(), bytes);
 
     let (tail, decoded_packet) = decoder::decode_udp(&bytes).expect("decode failed");
     assert_eq!(0, tail.len());
@@ -28,23 +26,7 @@ fn test_encode_message_wo_args() {
 }
 
 #[test]
-fn test_encode_empty_bundle() {
-    let packet = OscPacket::Bundle(OscBundle {
-        timetag: (4, 2).into(),
-        content: vec![],
-    });
-
-    let bytes = encoder::encode(&packet).unwrap();
-    assert_eq!(bytes.len() % 4, 0);
-    assert_eq!(bytes.len(), 16);
-
-    let (tail, decoded_packet) = decoder::decode_udp(&bytes).expect("decode failed");
-    assert_eq!(0, tail.len());
-    assert_eq!(packet, decoded_packet)
-}
-
-#[test]
-fn test_encode_message_with_args() {
+fn test_encode_message_with_all_types() {
     let packet = OscPacket::Message(OscMessage {
         addr: "/another/address/1".to_string(),
         args: vec![
@@ -89,8 +71,8 @@ fn test_encode_message_with_args() {
         ],
     });
 
-    let bytes = encoder::encode(&packet).unwrap();
-    assert_eq!(bytes.len() % 4, 0);
+    let bytes = encoder::encode(&packet).expect("encode failed");
+    assert_eq!(hex::decode(GOLDEN_MESSAGE_WITH_ALL_TYPES).unwrap(), bytes);
 
     let (tail, decoded_packet) = decoder::decode_udp(&bytes).expect("decode failed");
     assert_eq!(0, tail.len());
@@ -98,7 +80,22 @@ fn test_encode_message_with_args() {
 }
 
 #[test]
-fn test_encode_bundle() {
+fn test_empty_bundle() {
+    let packet = OscPacket::Bundle(OscBundle {
+        timetag: (4, 2).into(),
+        content: vec![],
+    });
+
+    let bytes = encoder::encode(&packet).expect("encode failed");
+    assert_eq!(hex::decode(GOLDEN_EMPTY_BUNDLE).unwrap(), bytes);
+
+    let (tail, decoded_packet) = decoder::decode_udp(&bytes).expect("decode failed");
+    assert_eq!(0, tail.len());
+    assert_eq!(packet, decoded_packet)
+}
+
+#[test]
+fn test_bundle() {
     let packet = OscPacket::Bundle(OscBundle {
         timetag: (1234, 4321).into(),
         content: vec![
@@ -126,16 +123,17 @@ fn test_encode_bundle() {
         ],
     });
 
-    let bytes = encoder::encode(&packet).unwrap();
-    assert_eq!(bytes.len() % 4, 0);
+    let bytes = encoder::encode(&packet).expect("encode failed");
+    assert_eq!(hex::decode(GOLDEN_BUNDLE).unwrap(), bytes);
 
-    let decoded_packet = decoder::decode_udp(&bytes).unwrap().1;
-    assert_eq!(packet, decoded_packet);
+    let (tail, decoded_packet) = decoder::decode_udp(&bytes).expect("decode failed");
+    assert_eq!(0, tail.len());
+    assert_eq!(packet, decoded_packet)
 }
 
 #[cfg(feature = "std")]
 #[test]
-fn test_encode_bundle_into_cursor() {
+fn test_bundle_cursor() {
     let packet = OscPacket::Bundle(OscBundle {
         timetag: (1234, 4321).into(),
         content: vec![
@@ -164,13 +162,11 @@ fn test_encode_bundle_into_cursor() {
     });
 
     let mut bytes = Vec::new();
-    encoder::encode_into(
+    let n = encoder::encode_into(
         &packet,
         &mut encoder::WriteOutput(std::io::Cursor::new(&mut bytes)),
     )
-    .unwrap();
-    assert_eq!(bytes.len() % 4, 0);
-
-    let decoded_packet = decoder::decode_udp(&bytes).unwrap().1;
-    assert_eq!(packet, decoded_packet);
+    .expect("encode failed");
+    assert_eq!(140, n);
+    assert_eq!(hex::decode(GOLDEN_BUNDLE).unwrap(), bytes);
 }
